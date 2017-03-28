410859c65c28b2dcef6f3718692b4fb96749ed45
edmund-h <447ght@gmail.com>
2017-03-28 14:26:57 -0400

    moved some stuff around

---
+++ Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  311 Tester
+//
+//  Created by Edmund Holderbaum on 3/25/17.
+//  Copyright © 2017 Bozo Design Labs. All rights reserved.
+//
+
+import Foundation
+
+enum Secrets: String{
+    case appID = "bca92893", appKey = "b8bb379ec7464d3ec227e527ef7b0da6"
+}
